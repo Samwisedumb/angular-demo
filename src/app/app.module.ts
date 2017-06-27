fd786e61dd721ac4e9803809b0077a313febbd89
djoshuac <joshua.dawson@aggiemail.usu.edu>
2017-06-26 18:13:16 -0600

    Basic Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { PaymentService } from './payments/shared/payment.service'
 
@@ -16,7 +17,13 @@ import { PaymentsComponent } from './payments/payments.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'payments',
+        component: PaymentsComponent
+      }
+    ])
   ],
   providers: [
     PaymentService
